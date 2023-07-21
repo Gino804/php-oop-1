20d7aabd468bb1a178af3f6e18690448b02e2cde
Gino804 <giacomo.p03@gmail.com>
2023-07-21 15:54:18 +0200

    Added actors

--- index.php
+++ index.php
@@ -1,16 +1,46 @@
 <?php
 
+class Actor
+{
+    public $id;
+    public $firstName;
+    public $lastName;
+    public $age;
+
+    function __construct($id, $firstName, $lastName, $age)
+    {
+        $this->id = $id;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->age = $age;
+    }
+
+    function getFullName()
+    {
+        return $this->firstName . ' ' . $this->lastName;
+    }
+}
+
+$actor1 = new Actor(12, 'Tom', 'Hanks', 67);
+$actor2 = new Actor(42, 'Stanley', 'Tucci', 62);
+$actor3 = new Actor(88, 'Mark', 'Ruffalo', 55);
+$actor4 = new Actor(127, 'Tom', 'Holland', 27);
+
 class Movie
 {
+    public $id;
     public $title;
     public $length;
     public $releaseYear;
+    public $cast;
 
-    function __construct($title, $length, $releaseYear)
+    function __construct($id, $title, $length, $releaseYear, array $cast)
     {
+        $this->id = $id;
         $this->title = $title;
         $this->length = $length;
         $this->releaseYear = $releaseYear;
+        $this->cast = $cast;
     }
 
     function setTitle($title)
@@ -19,8 +49,10 @@ class Movie
     }
 }
 
-$movie = new Movie('The Terminal', '123', '2004');
-$movie2 = new Movie('Avengers: Endgame', '182', '2019');
+$movie1 = new Movie(243, 'The Terminal', 123, 2004, [$actor1, $actor2]);
+$movie2 = new Movie(675, 'Avengers: Endgame', 182, 2019, [$actor3, $actor4]);
+
+$movies = [$movie1, $movie2];
 
 ?>
 
@@ -34,17 +66,22 @@ $movie2 = new Movie('Avengers: Endgame', '182', '2019');
 </head>
 
 <body>
-    <ul>
-        <li><b>Titolo: </b><?= $movie->title ?></li>
-        <li><b>Durata: </b><?= $movie->length ?> minuti</li>
-        <li><b>Anno di uscita: </b><?= $movie->releaseYear ?></li>
-    </ul>
-
-    <ul>
-        <li><b>Titolo: </b><?= $movie2->title ?></li>
-        <li><b>Durata: </b><?= $movie2->length ?> minuti</li>
-        <li><b>Anno di uscita: </b><?= $movie2->releaseYear ?></li>
-    </ul>
+    <?php foreach ($movies as $movie) : ?>
+        <ul>
+            <li><b>Titolo: </b><?= $movie->title ?></li>
+            <li><b>Durata: </b><?= $movie->length ?> minuti</li>
+            <li><b>Anno di uscita: </b><?= $movie->releaseYear ?></li>
+            <li><b>Cast: </b>
+                <?php
+                foreach ($movie->cast as $actor) {
+                    $paragraph = $actor->getFullName() . ' (' . $actor->age . ' anni)';
+                    if ($actor->id != $movie->cast[count($movie->cast) - 1]->id) $paragraph .= ', ';
+                    echo $paragraph;
+                }
+                ?>
+            </li>
+        </ul>
+    <?php endforeach ?>
 </body>
 
 </html>
